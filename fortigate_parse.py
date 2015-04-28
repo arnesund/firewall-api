27517e20edcb23f2f6e014a73cb52960c7277440
Arne Sund <arnesund@users.noreply.github.com>
2015-04-28 12:02:42 +0000

    Populate firewall metadata and save rules and data to shelve database

--- fortigate_parse.py
+++ fortigate_parse.py
@@ -336,6 +336,8 @@ def main(configfile, verbose):
         if section == 'router' and line[:12] == 'set hostname':
             obj[section]['hostname'] = line.split()[2]
             contents = obj[section]['hostname']
+            # Remove "" from hostname
+            contents = contents.replace("'", '')
             contents = contents.replace('"', '')
             obj[section]['hostname'] = contents
 
@@ -355,7 +357,6 @@ def main(configfile, verbose):
                 contents = obj[section][elem][title]
                 contents = contents.replace(' ', '/')
                 obj[section][elem][title] = contents
-            # Remove "" from hostname
 
     
     print obj['router']['hostname']
@@ -385,9 +386,56 @@ def main(configfile, verbose):
             else:
                 proto2rule[acl][rule.protocol].append(ruleindex)
                 
+    # Populate firewall metadata
+    hostname = obj['router']['hostname']
+    if hostname not in firewalls:
+        firewalls[hostname] = {}
+    for acl in accesslists:
+        intf = '-'.join([hostname.split('-')[1], acl.split('-')[0]])
+        firewalls[hostname][intf] = {'in': acl}
+
+
     # Debug print            
     if verbose > 1:        
         pprint(obj)    
+        print(proto2rule)
+        pprint(firewalls)
+        for acl in accesslists:
+            for rule in accesslists[acl]:
+                print(rule.ruleindex, acl, str(rule))
+
+
+    # Open output database
+    shelvefile = config['ACCESSLIST_DATABASE']
+    try:
+        db = shelve.open(shelvefile)
+        if 'accesslists' in db:
+            acldb = db['accesslists']
+        else:
+            acldb = {}
+        # Save firewall metadata
+        db['firewalls'] = firewalls
+    except KeyError as e:
+        logging.error('Unable to find database entry {0} in shelve file {1}'.format(e, shelvefile))
+        sys.exit(1)
+
+    # Save each access-list to output database
+    if hostname not in acldb:
+        acldb[hostname] = {}
+    for acl in accesslists:
+        if acl not in acldb[hostname]:
+            acldb[hostname][acl] = {}
+        acldb[hostname][acl]['rules'] = accesslists[acl]
+        acldb[hostname][acl]['timestamp'] = timestamp
+        acldb[hostname][acl]['protocols'] = proto2rule[acl]
+
+    # Close output database
+    db['accesslists'] = acldb
+    db.close()
+
+    # Return dict for easier testing and debugging in ipython
+    return accesslists
+
     
 if __name__ == '__main__':
     prog = os.path.basename(sys.argv[0])
